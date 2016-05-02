e34742c417497c15b855bcc901e6c7421577b71f
Skippy <mdskrzypczyk@gmail.com>
2016-05-01 19:09:45 -0500

    Organized files a little bit, moved bin2ascii, makeimage, and prngstreamgen to the util directory

--- bin_to_ascii.py
+++
@@ -1,4 +0,0 @@
-from sys import argv
-bin_data = open(argv[1]).read()
-ascii = ''.join(chr(int(bin_data[i:i+8],2)) for i in xrange(0, len(bin_data),8))
-print ascii
--- prngstreamgen.py
+++
@@ -1,8 +0,0 @@
-import random
-import sys
-outfile = str(sys.argv[1])
-f = open(outfile, 'w')
-for x in range(0,16384):
-	f.write(str(random.randint(0,1)))
-f.close
-
