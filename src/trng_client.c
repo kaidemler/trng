388d8eb594f55f269bd756934be493105dbfae55
Skippy <mdskrzypczyk@gmail.com>
2016-05-10 13:48:27 -0500

    Adding source

---
+++ src/trng_client.c
@@ -0,0 +1,182 @@
+/**
+ * Project: AVR ATtiny USB Tutorial at http://codeandlife.com/
+ * Author: Joonas Pihlajamaa, joonas.pihlajamaa@iki.fi
+ * Based on V-USB example code by Christian Starkjohann
+ * Copyright: (C) 2012 by Joonas Pihlajamaa
+ * License: GNU GPL v3 (see License.txt)
+ */
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+// this is libusb, see http://libusb.sourceforge.net/ 
+#include <usb.h>
+
+// same as in main.c
+#define USB_LED_OFF 0
+#define USB_LED_ON  1
+#define USB_DATA_PULL 2
+#define REPLYSIZE 64
+
+
+// used to get descriptor strings for device identification 
+static int usbGetDescriptorString(usb_dev_handle *dev, int index, int langid, 
+                                  char *buf, int buflen) {
+    char buffer[256];
+    int rval, i;
+
+	// make standard request GET_DESCRIPTOR, type string and given index 
+    // (e.g. dev->iProduct)
+	rval = usb_control_msg(dev, 
+        USB_TYPE_STANDARD | USB_RECIP_DEVICE | USB_ENDPOINT_IN, 
+        USB_REQ_GET_DESCRIPTOR, (USB_DT_STRING << 8) + index, langid, 
+        buffer, sizeof(buffer), 1000);
+        
+    if(rval < 0) // error
+		return rval;
+	
+    // rval should be bytes read, but buffer[0] contains the actual response size
+	if((unsigned char)buffer[0] < rval)
+		rval = (unsigned char)buffer[0]; // string is shorter than bytes read
+	
+	if(buffer[1] != USB_DT_STRING) // second byte is the data type
+		return 0; // invalid return type
+		
+	// we're dealing with UTF-16LE here so actual chars is half of rval,
+	// and index 0 doesn't count
+	rval /= 2;
+	
+	// lossy conversion to ISO Latin1 
+	for(i = 1; i < rval && i < buflen; i++) {
+		if(buffer[2 * i + 1] == 0)
+			buf[i-1] = buffer[2 * i];
+		else
+			buf[i-1] = '?'; // outside of ISO Latin1 range
+	}
+	buf[i-1] = 0;
+	
+	return i-1;
+}
+
+static usb_dev_handle * usbOpenDevice(int vendor, char *vendorName, 
+                                      int product, char *productName) {
+	struct usb_bus *bus;
+	struct usb_device *dev;
+	char devVendor[256], devProduct[256];
+    
+	usb_dev_handle * handle = NULL;
+	
+	usb_init();
+	usb_find_busses();
+	usb_find_devices();
+	
+	for(bus=usb_get_busses(); bus; bus=bus->next) {
+		for(dev=bus->devices; dev; dev=dev->next) {	
+		//	printf("VendorID: %x, ProductID: %x\n", dev->descriptor.idVendor, dev->descriptor.idProduct);	
+			if(dev->descriptor.idVendor != vendor ||
+               dev->descriptor.idProduct != product)
+                continue;
+                
+            // we need to open the device in order to query strings 
+            if(!(handle = usb_open(dev))) {
+                fprintf(stderr, "Warning: cannot open USB device: %s\n",
+                    usb_strerror());
+                continue;
+            }
+            
+            // get vendor name 
+            if(usbGetDescriptorString(handle, dev->descriptor.iManufacturer, 0x0409, devVendor, sizeof(devVendor)) < 0) {
+                fprintf(stderr, 
+                    "Warning: cannot query manufacturer for device: %s\n", 
+                    usb_strerror());
+                usb_close(handle);
+                continue;
+            }
+            
+            // get product name 
+            if(usbGetDescriptorString(handle, dev->descriptor.iProduct, 
+               0x0409, devProduct, sizeof(devVendor)) < 0) {
+                fprintf(stderr, 
+                    "Warning: cannot query product for device: %s\n", 
+                    usb_strerror());
+                usb_close(handle);
+                continue;
+            }
+            
+            if(strcmp(devVendor, vendorName) == 0 && 
+               strcmp(devProduct, productName) == 0)
+                return handle;
+            else
+                usb_close(handle);
+		}
+	}
+	
+	return NULL;
+}
+
+int main(int argc, char **argv) {
+	usb_dev_handle *handle = NULL;
+    int nBytes = 0;					// number of bytes pulled
+    char buffer[REPLYSIZE];			// device reply data
+
+	if(argc < 2) {
+		printf("Usage:\n");
+		printf("trng_client <command> <option>\n");
+		printf("on - Turn onboard LED on\n");
+		printf("off - Turn onboard LED off\n");
+		printf("pull <numBytes> - Pull numBytes of random bits from board\n");
+		exit(1);
+	}
+	
+	handle = usbOpenDevice(0x16C0, "mskd", 0x05DC, "TRNG1.0");
+	
+	if(handle == NULL) {
+		fprintf(stderr, "Could not find USB device!\n");
+		exit(1);
+	}
+
+	// turn LED on
+	if(strcmp(argv[1], "on") == 0) {
+		nBytes = usb_control_msg(handle, 
+            USB_TYPE_VENDOR | USB_RECIP_DEVICE | USB_ENDPOINT_IN, 
+			USB_LED_ON, 0, 0, (char *)buffer, sizeof(buffer), 5000);
+	}
+
+	// turn LED off
+	else if(strcmp(argv[1], "off") == 0) {
+		nBytes = usb_control_msg(handle, 
+            USB_TYPE_VENDOR | USB_RECIP_DEVICE | USB_ENDPOINT_IN, 
+			USB_LED_OFF, 0, 0, (char *)buffer, sizeof(buffer), 5000);	
+	} 
+
+	// get data
+	else if(strcmp(argv[1], "pull") == 0 && argc > 2) {
+		int b = atoi(argv[2]);		// number user wants
+		while(b >= 0)
+		{
+			// pull
+			if(b >= REPLYSIZE) nBytes = usb_control_msg(handle, USB_TYPE_VENDOR | USB_RECIP_DEVICE | USB_ENDPOINT_IN, USB_DATA_PULL, 0, 0, (char *)buffer, REPLYSIZE, 5000);
+			else nBytes = usb_control_msg(handle, USB_TYPE_VENDOR | USB_RECIP_DEVICE | USB_ENDPOINT_IN, USB_DATA_PULL, 0, 0, (char *)buffer, b, 5000);
+			
+			// error check
+			if(nBytes < 0){
+				break;
+			}
+
+			// write to the screen for piping
+			fwrite(buffer, nBytes, sizeof(char), stdout);
+			fflush(stdout);
+
+			// decrement
+			b -= REPLYSIZE;
+		}
+	}
+
+	// alert error
+	if(nBytes < 0)
+		fprintf(stderr, "USB error: %s\n", usb_strerror());
+		
+	usb_close(handle);
+	
+	return 0;
+}
